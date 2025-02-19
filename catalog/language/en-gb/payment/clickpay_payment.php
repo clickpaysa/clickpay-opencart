6265c25ef63a5c4e9fadc347b225e8197514218a
Riyaztechsupport <135695828+Riyaztechsupport@users.noreply.github.com>
2025-02-19 12:21:33 +0300

    Delete catalog directory

--- catalog/language/en-gb/payment/clickpay_payment.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Heading
-$_['heading_title']      = 'Clickpay Credit Card Payment';
-
-// Text
-$_['button_confirm'] = 'Pay';
-$_['text_title'] = 'ClickPay';
-$_['text_description'] = 'Pay using ClickPay.';
-
