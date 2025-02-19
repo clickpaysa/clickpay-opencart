e40d3b941582fabd1f0ead3df34230fec9724356
Riyaztechsupport <135695828+Riyaztechsupport@users.noreply.github.com>
2025-02-19 12:17:32 +0300

    Delete admin directory

--- admin/language/en-gb/payment/clickpay_mada.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'ClickPay - mada';
-
-// Text
-$_images_url = HTTP_CATALOG . 'extension/clickpay/admin/view/image/';
-$_['text_clickpay_mada'] = '<img src="' . $_images_url . 'mada.png" alt="ClickPay - mada" title="ClickPay - mada" style="max-width: 90px; max-height: 70px;" />';
--- admin/language/en-gb/payment/clickpay_sadad.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'ClickPay - Sadad';
-
-// Text
-$_images_url = HTTP_CATALOG . 'extension/clickpay/admin/view/image/';
-$_['text_clickpay_sadad'] = '<img src="' . $_images_url . 'sadad.png" alt="ClickPay - Sadad" title="ClickPay - SADAD" style="max-width: 90px; max-height: 70px;" />';
